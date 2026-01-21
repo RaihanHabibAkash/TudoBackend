e65850cb2c55b42ac3de7b39988991327fcb9507
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-01-21 13:15:55 +0600

    feat: add user model and dependencies
    - Added user model with fields: userName, email, password, profilePic, tudos, and subTudos.
    - Updated package.json to include express and mongoose as dependencies.

---
+++ src/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profilePic: {
+        type: String,
+        required: false,
+        unique: true
+    },
+    tudos: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tudo",
+        required: false
+    },
+    subTudos: {
+        type: mongoose.Schema.Types.ObjectId,
+        red: "SubTudo",
+        required: false
+    }
+
+    
+} , { timestamps: true });
+
+export const User = ("User", userSchema);
